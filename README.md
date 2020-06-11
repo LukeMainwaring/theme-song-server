9f411b0a2dd03fcfdc7558bbe6b69f409ed81d39
Luke Mainwaring <lukemainwaring@Lukes-MacBook-Pro-3.local>
2020-06-10 23:08:28 -0400

    random words generator for create game

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 Docker scripts to run:
 \$ docker-compose up
 
+If a new package is installed
+\$ docker-compose build
+\$ docker-compose up
+
 To stop:
 Ctrl + c,
 \$ docker-compose down
--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const randomWords = require('random-words');
 
 const router = express.Router();
 
 router.get('/createGame', (req, res) => {
-  res.send({ data: 'word1-word2' });
+  res.send({ words: randomWords({ exactly: 2, join: '-' }) });
 });
 
 module.exports = router;
